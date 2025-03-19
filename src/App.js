15dd1fcd394417b979d2058a9e08d470b5c605d2
Dmitriy Solovev <dmptts@gmail.com>
2025-03-19 10:01:04 +0300

    feat: верстает страницу логина

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import * as PageTemplates from './pages';
 import Handlebars from 'handlebars';
 import Input from './components/Input';
 import Button from "./components/Button";
+
+const PAGES = {
+  Login: 'Login',
+}
+
 Handlebars.registerPartial('Button', Button)
 Handlebars.registerPartial('Input', Input);
 
 export default class App {
   constructor() {
     this.state = {
-      currentPage: '',
+      currentPage: PAGES.Login,
     };
     this.appElement = document.getElementById('app');
   }
 
-  render() {}
+  render() {
+    switch (this.state.currentPage) {
+      case PAGES.Login:
+        this.appElement.innerHTML = Handlebars.compile(PageTemplates.LoginPage)();
+        break;
+      default:
+        break;
+    }
+  }
 }
