74d790bed1367be01c40b918ff7a95ca8cb6f4db
Dmitriy Solovev <dmptts@gmail.com>
2025-03-19 09:45:07 +0300

    feat(elements): добавляет шаблон кнопки

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import Handlebars from 'handlebars';
+import Button from "./components/Button";
+Handlebars.registerPartial('Button', Button)
 export default class App {
   constructor() {
     this.state = {
