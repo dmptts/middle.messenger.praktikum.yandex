95f0d84f7ed82497b24e421ba24d231da83ff88c
Dmitriy Solovev <dmptts@gmail.com>
2025-03-19 10:00:15 +0300

    feat(elements): добавляет шаблон поля ввода

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Handlebars from 'handlebars';
+import Input from './components/Input';
 import Button from "./components/Button";
 Handlebars.registerPartial('Button', Button)
+Handlebars.registerPartial('Input', Input);
+
 export default class App {
   constructor() {
     this.state = {
