6e0aea47245e4e7451cfe2eeedf2223a7718aada
Dmitriy Solovev <dmptts@gmail.com>
2025-05-01 16:50:10 +0300

    feat: переводит Button на использование Component

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,3 +1,24 @@
-import template from './Button.hbs?raw';
+import Component from '../../services/Component';
+import { BaseProps } from '../../utils/types';
+import template from "./template.hbs?raw";
 
-export default template;
+interface ButtonProps extends BaseProps {
+  text: string;
+  onClick?: EventListener;
+  type?: 'button' | 'submit';
+  className?: string;
+  disabled?: boolean;
+}
+
+export default class Button extends Component<ButtonProps> {
+  constructor(props: ButtonProps) {
+    super({
+      ...props,
+      type: props.type ?? 'button',
+    });
+  }
+
+  render() {
+    return this.compile(template);
+  }
+}
