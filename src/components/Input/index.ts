785b0281f3c99ea7decbe4782bbe1d9b471d3347
Dmitriy Solovev <dmptts@gmail.com>
2025-05-01 16:52:36 +0300

    feat: переводит Input на использование Component

--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,3 +1,81 @@
-import template from './Input.hbs?raw';
+import Component, { eventTypesMap } from '../../services/Component';
+import { BaseProps, Nullable } from '../../utils/types';
+import template from './template.hbs?raw';
 
-export default template;
+interface InputProps extends BaseProps {
+  type: string;
+  name: string;
+  id?: string;
+  label?: string;
+  value?: string;
+  required?: boolean;
+  onBlur?: EventListener;
+  onClick?: EventListener;
+  validation?: (value: string) => Nullable<string>;
+}
+
+export default class Input extends Component<InputProps> {
+  constructor(props: InputProps) {
+    super(props);
+  }
+
+  render() {
+    return this.compile(template);
+  }
+
+  validate() {
+    const input = this._element?.querySelector('input') as HTMLInputElement;
+    const errorContainer = this.element?.querySelector('.input-component__error');
+
+    if (!input || !errorContainer) {
+      return null;
+    }
+
+    const error = this.props?.validation?.(input.value.trim()) ?? null;
+    errorContainer.textContent = error ?? '';
+
+    return error;
+  }
+
+  addEvents() {
+    if (!this.element) {
+      throw new Error(
+        `Элемент еще не создан`,
+      );
+    }
+
+    const input = this.element.querySelector('input');
+
+    if (!input) {
+      return;
+    }
+
+    for (const listener in this._listeners) {
+      const handler = this._listeners[listener as keyof InputProps];
+
+      if (!handler) {
+        return;
+      }
+
+      input.addEventListener(
+        eventTypesMap[listener as keyof typeof eventTypesMap],
+        handler.bind(this),
+      );
+    }
+  }
+
+  removeEvents() {
+    const input = this.element?.querySelector('input');
+
+    if (!input || !this._listeners) {
+      return;
+    }
+
+    for (const listener in this._listeners) {
+      input.removeEventListener(
+        eventTypesMap[listener as keyof typeof eventTypesMap],
+        this._listeners[listener as keyof InputProps]
+      );
+    }
+  }
+}
