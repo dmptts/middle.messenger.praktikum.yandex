a50914302934d3d2619f11c6d031b30b7acd29e2
Dmitriy Solovev <dmptts@gmail.com>
2025-07-26 13:04:09 +0300

    fix: добавляет переход к мессенджеру со страницы ошибки сервера

--- src/pages/ServerErrorPage/index.ts
+++ src/pages/ServerErrorPage/index.ts
@@ -2,6 +2,7 @@ import Link from '../../components/Link';
 import Component from '../../services/Component';
 import { BaseProps } from '../../utils/types';
 import template from './template.hbs?raw';
+import { Routes } from "../../App";
 
 interface ServerErrorPageProps extends BaseProps {
   link: Link;
@@ -12,6 +13,7 @@ export default class ServerErrorPage extends Component<ServerErrorPageProps> {
     super({
       link: new Link({
         text: 'Назад к чатам',
+        to: Routes.Messenger,
       }),
     });
   }
